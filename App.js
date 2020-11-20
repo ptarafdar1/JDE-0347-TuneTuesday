c983f436eaa2465e1e27f867659745fda31e2c0e
ptarafdar1 <tarafdarpritish842@gmail.com>
2020-11-20 14:33:07 -0500

    sebastian edits

--- App.js
+++ App.js
@@ -1,15 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
-import favicon from './assets/favicon.png';
-
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+import { Header } from 'react-native-elements';
+import logo from './assets/favicon.png';
+import { Ionicons } from '@expo/vector-icons';
+//const backArrow = <Ionicons name="md-arrow-back" size={30} color="#fff"/>;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={favicon} style={{ width: 305, height: 300}}></Image>
-      <Text style={{color:'#888', fontSize: 18}}>Hello World!</Text>
+      <Header
+        leftComponent={<Ionicons name="md-arrow-back" size={30} color="#fff"/>}
+        centerComponent={{ text: 'Game Name', style: { color: '#fff', fontSize: 30 } }}
+        rightComponent={{ icon: 'home', color: '#fff' }}
+      />
+      <Text style =
+      {{
+        color: '#000',
+        paddingTop: 40,
+        paddingBottom: 80,
+        fontSize: 40
+      }}>Submit a Song!</Text>
       <StatusBar style="auto" />
+    <TextInput
+      style={{
+      height: 40,
+      width: 250,
+      color: "#000",
+      borderColor: '#000',
+      borderWidth: 2,
+      
+    }}
+    placeholder=" Paste Link Here"
+    />
+    <TextInput
+      style={{
+      height: 40,
+      width: 250,
+      color: "#000",
+      borderColor: '#000',
+      borderWidth: 2,
+      
+    }}
+    placeholder=" Song Name"
+    />
+    <TextInput
+      style={{
+      height: 40,
+      width: 250,
+      color: "#000",
+      borderColor: '#000',
+      borderWidth: 2,
+      
+    }}
+    placeholder=" Artist Name"
+    />
+    
+    <Button
+      onPress={() => Alert.alert("Submitted Song")}
+      title= "Submit"
+      color = "#000000"
+      />
+    <Text style =
+      {{
+        color: '#000',
+        paddingTop: 40,
+        paddingBottom: 10,
+        fontSize: 20
+      }}>Search on YouTube</Text>
+    <Text style =
+      {{
+        color: '#000',
+        paddingTop: 10,
+        paddingBottom: 80,
+        fontSize: 20
+      }}>Search on Spotify</Text>
     </View>
   );
 }
@@ -19,6 +84,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center',
   },
 });
