09cf851c034037581d1a4f5aa3f6c861290d0089
ptarafdar1 <tarafdarpritish842@gmail.com>
2020-11-20 18:00:58 -0500

    Fixed margins and padding

--- App.js
+++ App.js
@@ -14,67 +14,86 @@ export default function App() {
         centerComponent={{ text: 'Game Name', style: { color: '#fff', fontSize: 30 } }}
         rightComponent={{ icon: 'home', color: '#fff' }}
       />
+
       <Text style =
       {{
         color: '#000',
         paddingTop: 40,
         paddingBottom: 80,
         fontSize: 40
-      }}>Submit a Song!</Text>
+      }}>Submit a Song!
+      </Text>
+
       <StatusBar style="auto" />
-    <TextInput
-      style={{
-      height: 40,
-      width: 250,
-      color: "#000",
-      borderColor: '#000',
-      borderWidth: 2,
-      
-    }}
-    placeholder=" Paste Link Here"
-    />
-    <TextInput
-      style={{
-      height: 40,
-      width: 250,
-      color: "#000",
-      borderColor: '#000',
-      borderWidth: 2,
-      
-    }}
-    placeholder=" Song Name"
-    />
-    <TextInput
-      style={{
-      height: 40,
-      width: 250,
-      color: "#000",
-      borderColor: '#000',
-      borderWidth: 2,
-      
-    }}
-    placeholder=" Artist Name"
-    />
-    
-    <Button
-      onPress={() => Alert.alert("Submitted Song")}
-      title= "Submit"
-      color = "#000000"
-      />
-    <Text style =
-      {{
-        color: '#000',
-        paddingTop: 40,
-        paddingBottom: 10,
-        fontSize: 20
-      }}>Search on YouTube</Text>
-    <Text style =
-      {{
-        color: '#000',
+
+      <TextInput
+        style={{
+        height: 40,
+        width: 250,
+        color: "#000",
+        borderColor: '#000',
+        borderWidth: 2,
         paddingTop: 10,
-        paddingBottom: 80,
-        fontSize: 20
-      }}>Search on Spotify</Text>
+        paddingBottom: 10,
+        paddingLeft: 10,
+        marginBottom: 20,
+      }}
+      placeholder=" Paste Link Here"
+      />
+
+      <TextInput
+        style={{
+        height: 40,
+        width: 250,
+        color: "#000",
+        borderColor: '#000',
+        borderWidth: 2,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 10,
+        marginBottom: 20,
+      }}
+      placeholder=" Song Name"
+      />
+
+      <TextInput
+        style={{
+        height: 40,
+        width: 250,
+        color: "#000",
+        borderColor: '#000',
+        borderWidth: 2,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 10,
+        marginBottom: 20,
+      }}
+      placeholder=" Artist Name"
+      />
+    
+      <Button
+        onPress={() => Alert.alert("Submitted Song")}
+        title= "Submit"
+        color = "#000000"
+        />
+
+      <Text style =
+        {{
+          color: '#000',
+          paddingTop: 40,
+          paddingBottom: 10,
+          fontSize: 20
+        }}>Search on YouTube
+      </Text>
+
+      <Text style =
+        {{
+          color: '#000',
+          paddingTop: 10,
+          paddingBottom: 80,
+          fontSize: 20
+        }}>Search on Spotify
+      </Text>
     </View>
   );
 }
